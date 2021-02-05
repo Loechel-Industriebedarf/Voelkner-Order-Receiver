4b9be62f3e8492bda5b078c55399dd9aa105efda
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2021-02-05 10:02:10 +0100

    Addes vs-code workspace

--- settings.sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$api_url = "https://rein-prod.mirakl.net/api";
-	$api_key = "";
-	
-	$csvPath = "../voelknerOrder.csv";
-	$last_execution = file_get_contents('last.txt');
-	
-	date_default_timezone_set("Europe/Berlin");
